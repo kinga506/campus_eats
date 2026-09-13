dca37d7e583b4dbe95515dd2c6f48654c1b7544f
kinga506 <kingadorji884@gmail.com>
2026-09-13 11:27:08 +0600

    Unit IV Lab 3: PostgreSQL connection, Restaurant and MenuItem models, restaurant-specific menus

--- app.js
+++ app.js
@@ -17,6 +17,11 @@ app.use(express.urlencoded({ extended: true }));
 // Routes
 const indexRoutes = require('./routes/index');
 app.use('/', indexRoutes);
+const db = require('./config/db');
+app.get('/db-test', async (req, res) => {
+  const result = await db.one('SELECT NOW() AS current_time');
+  res.json(result);
+});
 
 app.listen(PORT, () => {
   console.log(`Campus Eats running at http://localhost:${PORT}`);
